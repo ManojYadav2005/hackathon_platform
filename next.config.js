80f5cedc86abc1209337d33e8c0f87c6edb96eda
manoj yadav <manojyadav@MacBookAir.mshome.net>
2025-11-15 20:05:21 +0530

    Fix: Add webpack config to fix Netlify undici build error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer, webpack }) => {
+    // This is the fix for the 'undici' module parse error during 'next build'
+    // It tells Webpack to treat 'undici' as an external module
+    // that shouldn't be bundled.
+    if (!isServer) {
+      config.externals = [...config.externals, 'undici'];
+    }
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
